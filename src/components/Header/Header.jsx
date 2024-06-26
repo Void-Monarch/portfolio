0a73d4c2612846527010edd7367d7a7218ed2718
VoidMonarch <hardcoregamer2232003@gmail.com>
2024-06-26 23:15:07 +0530

    home logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import NavDrawer from "./navbarDrawer";
 import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="flex flex-row justify-evenly">
       <div>
-        <img src={logo} className="h-8" />
+        <Link to="/">
+          <img src={logo} className="h-8" />
+        </Link>
       </div>
       <NavDrawer />
     </div>
