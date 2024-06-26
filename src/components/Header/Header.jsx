120400769947f8a40678b6b473953fa4040bb847
VoidMonarch <hardcoregamer2232003@gmail.com>
2024-06-26 23:14:41 +0530

    Home Logo added to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import NavDrawer from "./navbarDrawer";
+import logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex flex-row justify-evenly">
+      <div>
+        <img src={logo} className="h-8" />
+      </div>
       <NavDrawer />
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import monarch from "../../assets/Monarch-main-pic.png";
 export default function Home() {
   return (
     <div className="flex pt-64 justify-center">
-      <img src={monarch} alt="monarch" className="w-[90%] max-w-[1100px]" />
+      <img src={monarch} id="main_pic" alt="monarch" className="w-[90%] max-w-[1100px]" />
     </div>
   );
 }
