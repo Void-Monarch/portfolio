85ecc05f8cbd50005c621ab73814a0c95612a79c
VoidMonarch <hardcoregamer2232003@gmail.com>
2024-06-27 09:06:45 +0530

    font 0.2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,15 @@ import monarch from "../../assets/Monarch-main-pic.png";
 
 export default function Home() {
   return (
-    <div className="flex pt-64 justify-center">
-      <img src={monarch} id="main_pic" alt="monarch" className="w-[90%] max-w-[1100px]" />
-      <h1 className="font-RobotoCondensed">
-        Hello
-      </h1>
+    <div className="flex justify-center pt-64">
+      <img
+        src={monarch}
+        id="main_pic"
+        alt="monarch"
+        className="w-[90%] max-w-[1100px]"
+      />
+      <br />
+      <h1 className="font-RobotoCondensed text-3xl text-white">Hello</h1>
     </div>
   );
 }
