6f1b06f83f3d4e6e1b04833e57258cdeb4bb0ba4
VoidMonarch <hardcoregamer2232003@gmail.com>
2024-06-27 09:04:17 +0530

    font 0.1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className="flex pt-64 justify-center">
       <img src={monarch} id="main_pic" alt="monarch" className="w-[90%] max-w-[1100px]" />
+      <h1 className="font-RobotoCondensed">
+        Hello
+      </h1>
     </div>
   );
 }
