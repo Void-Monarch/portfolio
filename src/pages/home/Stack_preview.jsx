28d614d5eea1c946b12ae21eb3261f42198d638e
VoidMonarch <hardcoregamer2232003@gmail.com>
2024-07-07 14:16:16 +0530

    tech stack update

--- src/pages/home/Stack_preview.jsx
+++ src/pages/home/Stack_preview.jsx
@@ -3,12 +3,22 @@ import Tag_glow_button from "../../components/Tag_glow_button";
 import { useEffect, useRef } from "react";
 import anime from "animejs";
 import { useInView } from "react-intersection-observer";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import jslogo from "../../assets/JavaScript-logo.png";
+import pylogo from "../../assets/Python_icon.png";
+import reactlogo from "../../assets/react-icon.png";
+import nextlogo from "../../assets/nextlogo.webp";
+import rustlogo from "../../assets/rustlogo.webp";
+import tailwindlogo from "../../assets/tailwindlogo.jpg";
 
 export default function Stack_preview() {
   const lines = useRef(null);
   const [ref, inView] = useInView({
     threshold: 0.3,
   });
+
   useEffect(() => {
     const t2 = anime({
       targets: document.querySelector(".stack_preview_div"),
@@ -39,6 +49,27 @@ export default function Stack_preview() {
     }
   }, [inView]);
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      partialVisibilityGutter: 30,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      partialVisibilityGutter: 30,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      partialVisibilityGutter: 30,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   return (
     <div
       className="stack_preview_div flex w-full justify-center opacity-0 sm:mx-auto sm:max-w-[100rem]"
@@ -62,9 +93,71 @@ export default function Stack_preview() {
               </div>
             </div>
           </h2>
-          <div>
-            
-          </div>
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            swipeable={true}
+            draggable={true}
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            className="my-20"
+            autoPlay={true}
+            // partialVisible={true}
+            autoPlaySpeed={1000}
+            centerMode={true}
+            arrows={false}
+            customTransition="transition-all  duration-500"
+            itemClass="rounded-xl p-5"
+            transitionDuration={500}
+          >
+            <div className="item text-center">
+              <img
+                src={jslogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>JavaScript</h5>
+            </div>
+            <div className="item text-center">
+              <img
+                src={pylogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>Python</h5>
+            </div>
+            <div className="item text-center">
+              <img
+                src={reactlogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>React</h5>
+            </div>
+            <div className="item text-center">
+              <img
+                src={nextlogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>Next JS</h5>
+            </div>
+            <div className="item text-center">
+              <img
+                src={tailwindlogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>TailWind CSS</h5>
+            </div>
+            <div className="item text-center">
+              <img
+                src={rustlogo}
+                alt="Image"
+                className="aspect-square object-cover rounded-xl"
+              />
+              <h5>Rust</h5>
+            </div>
+          </Carousel>
 
           <Link to="https://github.com/Void-Monarch?tab=repositories">
             <Tag_glow_button text="MORE" className="max-w-80" />
